b5d2616a8a0a6972dbb5df40a847cb378802e1c2
BhavikVashi1804 <bhavikvashi1804@gmail.com>
2019-11-07 18:03:52 +0530

    two dice

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,13 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         Expanded(
           child: Image(image: AssetImage('images/dice1.png'),),
+        ),
+        Expanded(
+          child: Image(image: AssetImage('images/dice2.png'),),
         ),
       ],
     );
